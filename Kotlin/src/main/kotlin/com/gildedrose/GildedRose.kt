0fd69e6f1f0505bc2b4b94c93b4fde2e4ba4f558
Viktoriya Hristova <69354797+vikihh@users.noreply.github.com>
2025-07-07 09:49:37 +0100

    final refactoring

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,44 +4,38 @@ class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name == "Sulfuras, Hand of Ragnaros") item.quality = 80;
-            else {
-                    if (item.name == "Aged Brie")
-                        item.quality  = item.quality + 1
-                    else {
-                        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                            item.quality = item.quality - 1
-                            if (item.name.startsWith("Conjured")) item.quality -= 1
-                        }
-                        else {
-                            item.quality ++
+            when (item.name)
+            {
+                "Sulfuras, Hand of Ragnaros" -> item.quality = 80
+                "Aged Brie" -> item.quality ++
+                "Backstage passes to a TAFKAL80ETC concert" -> item.quality ++
+                else -> item.quality --
+            }
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                if (item.name.startsWith("Conjured")) item.quality--
 
-                            if (item.sellIn < 11)
-                                item.quality ++
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn <= 10)
+                        item.quality++
 
-                            if (item.sellIn < 6)
-                                item.quality ++
-                        }
-                    }
+                    if (item.sellIn <= 5)
+                        item.quality++
 
-                    item.sellIn --
-
-                    if (item.sellIn < 0)
-                    {
-                        if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-                                        item.quality = 0
-                        else
-                            if (item.name == "Aged Brie")
-                                item.quality ++
-                            else {
-                                item.quality --
-                                if (item.name.startsWith("Conjured")) item.quality --
-                            }
-                    }
-
-                    if (item.quality > 50) item.quality = 50;
-                    if (item.quality < 0) item.quality = 0;
                 }
+
+                if (item.sellIn <= 0) {
+                    when (item.name) {
+                        "Aged Brie" -> item.quality++
+                        "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
+                        else -> item.quality--
+                    }
+                    if (item.name.startsWith("Conjured")) item.quality--
+
+                }
+                item.sellIn--
+                if (item.quality > 50) item.quality = 50;
+                if (item.quality < 0) item.quality = 0;
+            }
         }
     }
 }
