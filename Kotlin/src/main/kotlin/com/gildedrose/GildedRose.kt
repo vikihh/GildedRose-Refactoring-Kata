fe79eadc9dc224539bf0523e73db724bdd9ee6ee
Viktoriya Hristova <69354797+vikihh@users.noreply.github.com>
2025-07-07 15:10:50 +0100

    Update GildedRose.kt

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,39 +4,42 @@ class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            when (item.name)
+            when
             {
-                "Sulfuras, Hand of Ragnaros" -> item.quality = 80
-                "Aged Brie" -> item.quality ++
-                "Backstage passes to a TAFKAL80ETC concert" -> item.quality ++
-                else -> item.quality --
-            }
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                if (item.name.startsWith("Conjured")) item.quality--
-
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                item.name =="Sulfuras, Hand of Ragnaros" ->
+                {
+                    item.quality = 80
+                }
+                item.name == "Aged Brie" -> {
+                    item.quality ++
+                    if (item.sellIn <= 0) item.quality++
+                }
+                item.name == "Backstage passes to a TAFKAL80ETC concert" -> {
+                    item.quality ++
                     if (item.sellIn <= 10)
                         item.quality++
 
                     if (item.sellIn <= 5)
                         item.quality++
 
+                    if (item.sellIn <=0) item.quality =0
                 }
-
-                if (item.sellIn <= 0) {
-                    when (item.name) {
-                        "Aged Brie" -> item.quality++
-                        "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
-                        else -> item.quality--
-                    }
-                    if (item.name.startsWith("Conjured")) item.quality--
-
+                item.name.startsWith("Conjured") -> {
+                    item.quality -= 2
+                    if (item.sellIn<=0) item.quality -= 2
                 }
-                item.sellIn--
-                if (item.quality > 50) item.quality = 50;
-                if (item.quality < 0) item.quality = 0;
+                else -> {
+                    item.quality --
+                    if (item.sellIn <=0) item.quality --
+                }
             }
+
+            item.sellIn--
+            if (item.quality > 50 && item.name != "Sulfuras, Hand of Ragnaros") item.quality = 50;
+            if (item.quality < 0) item.quality = 0;
         }
     }
 }
+
+
 
